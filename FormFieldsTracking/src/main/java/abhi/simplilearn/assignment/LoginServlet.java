d7d3ca40caf9e8def1f22e18bb1b94426b1baf2f
Abhinav.kumar <Abhinav.kumar@ABHINAVK-LT.esolutions.local>
2022-01-13 12:06:50 +0530

    FormFieldTracking

---
+++ FormFieldsTracking/src/main/java/abhi/simplilearn/assignment/LoginServlet.java
@@ -0,0 +1,43 @@
+package abhi.simplilearn.assignment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+
+		String userId = request.getParameter("userid");
+		out.println("<form action='dashboard' method='post'>");
+		out.println("<input type='hidden' name='userid' id='userid' value='" + userId + "'>");
+		out.println("<input type='submit' value='submit' >");
+		out.println("</form>");
+		out.println("<script>document.forms[0].submit();</script>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
